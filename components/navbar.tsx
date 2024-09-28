2da6028a63e0aca4a67d8391d97f46eded8eddcc
Techo10n <trafficjammisterowl@gmail.com>
2024-09-28 03:37:21 -0400

    updated home page - proper input, header, etc.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,19 +8,26 @@ const Navbar: FC = () => {
   return (
     <nav className="p-4 text-white">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">shaz</h1>
+        <h1 className="text-xl font-bold">
+          <a href="#">
+            <svg className="w-4 h-4 inline-block" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+            </svg>
+          </a>
+          <span className="pl-2"><a href='/'>shaz</a></span>
+        </h1>
         <div className="relative">
           <button onClick={() => setDropdownOpen(!isDropdownOpen)} className="flex items-center space-x-2">
-            <span>Profile</span>
+            <span>(())</span>
             <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
             </svg>
           </button>
           {isDropdownOpen && (
-            <div className="absolute right-0 mt-2 w-48 bg-white rounded shadow-lg">
-              <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Dashboard</a>
-              <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Settings</a>
-              <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">Log out</a>
+            <div className="absolute right-0 mt-2 bg-[#191919] rounded-lg shadow-lg">
+              <a href="#" className="text-right block px-4 py-2 text-white hover:bg-[#cab0f5] rounded-lg">Profile</a>
+              <a href="#" className="text-right block px-4 py-2 text-white hover:bg-[#cab0f5] rounded-lg">Dashboard</a>
+              <a href="#" className="text-right block px-4 py-2 text-white hover:bg-[#cab0f5] rounded-lg">Log Out</a>
             </div>
           )}
         </div>
